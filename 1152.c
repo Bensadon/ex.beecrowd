cab43a5609844c7f790a6b23b6d95a6bc0b587db
David Bensadon <david.abraham@unifesp.br>
2025-12-09 16:44:04 -0300

    Create 1152.c

---
+++ 1152.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ESTRADAS 200005
+#define MAX_JUNCOES 200005
+
+typedef struct {
+    int u, v, peso;
+} Aresta;
+
+Aresta arestas[MAX_ESTRADAS];
+int pai[MAX_JUNCOES];
+
+int comparar(const void *a, const void *b) {
+    return ((Aresta*)a)->peso - ((Aresta*)b)->peso;
+}
+
+int buscar(int i) {
+    if (pai[i] == i)
+        return i;
+    return pai[i] = buscar(pai[i]);
+}
+
+void unir(int i, int j) {
+    int raiz_i = buscar(i);
+    int raiz_j = buscar(j);
+    if (raiz_i != raiz_j) {
+        pai[raiz_i] = raiz_j;
+    }
+}
+
+int main() {
+    int m, n;
+    int i;
+    int custo_total, custo_mst;
+
+    while (scanf("%d %d", &m, &n) && (m != 0 || n != 0)) {
+        custo_total = 0;
+        
+        for (i = 0; i < n; i++) {
+            scanf("%d %d %d", &arestas[i].u, &arestas[i].v, &arestas[i].peso);
+            custo_total += arestas[i].peso;
+        }
+
+        for (i = 0; i < m; i++) {
+            pai[i] = i;
+        }
+
+        qsort(arestas, n, sizeof(Aresta), comparar);
+
+        custo_mst = 0;
+        for (i = 0; i < n; i++) {
+            if (buscar(arestas[i].u) != buscar(arestas[i].v)) {
+                unir(arestas[i].u, arestas[i].v);
+                custo_mst += arestas[i].peso;
+            }
+        }
+
+        printf("%d\n", custo_total - custo_mst);
+    }
+
+    return 0;
+}
